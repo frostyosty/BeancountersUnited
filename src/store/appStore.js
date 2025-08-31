1b7eeecd92499589b048fbc827090340f8cd2a0b
frostyosty <92064099+frostyosty@users.noreply.github.com>
2025-08-31 01:21:17 +0000

    standard commit before this is stable

--- src/store/appStore.js
+++ src/store/appStore.js
@@ -1,18 +1,13 @@
 // src/store/appStore.js
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
-
-// We are only importing the menu slice for this test
 import { createMenuSlice } from './menuSlice.js';
 
 export const useAppStore = create(
   devtools(
     (set, get) => ({
-      // The entire store only has one namespace: 'menu'
-      menu: createMenuSlice(set, get),
+      ...createMenuSlice(set, get),
     }),
-    { name: "RestaurantAppStore (Menu Only)" }
+    { name: "RestaurantAppStore" }
   )
 );
-
-window.useAppStore = useAppStore;
